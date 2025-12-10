6ac3f5037480d6e06b647df78b2d9203bbef1f4f
Vinnyfio04 <vinny.fiorenza.2004@gmail.com>
2025-12-10 15:00:06 -0500

    Update main.js
    made camera set position whole numbers for clarity

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import { PointerLockControls } from 'three/addons/controls/PointerLockControls.j
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-camera.position.set(-13.828, 9.834, 32.849)
+camera.position.set(-13, 9, 32)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
